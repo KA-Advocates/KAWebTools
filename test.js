cb1846c17de668e9a7229a6eecdf525f31d3eda8
Uli Köhler <ulikoehler@online.de>
2017-10-26 06:32:51 +0200

    Added more tests

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ function assertNotTranslated(engl) {
     }
 }
 
-
+// Constructed strings
 assertSame("$a$")
 assertSame(">$a$")
 assertSame(" $b$ ")
@@ -28,9 +28,11 @@ assertSame("$d\\\\text{cm}$")
 assertSame("$d\\\\text{g}$")
 assertSame("$d\\\\text{m}$")
 
+// Real strings
 assertSame("web+graphie://ka-perseus-graphie.s3.amazonaws.com/b8ca00d508c9e7b593c669977fdde31570195273")
 assertSame("https://ka-perseus-images.s3.amazonaws.com/b8ca00d508c9e7b593c669977fdde31570195273.svg")
 assertSame("![](https://ka-perseus-images.s3.amazonaws.com/b8ca00d508c9e7b593c669977fdde31570195273.svg)")
 assertSame("https://ka-perseus-images.s3.amazonaws.com/b8ca00d508c9e7b593c669977fdde31570195273.png")
 assertSame("$\\\\blue{A_c} = \\\\pi (\\\\pink{10})^2$\\n\\n $\\\\blue{A_c} = 100\\\\pi$\\n\\n ![](web+graphie://ka-perseus-graphie.s3.amazonaws.com/b8ca00d508c9e7b593c669977fdde31570195273)")
 assertSame(">$\\\\pink{\\\\text{m}\\\\angle D} + \\\\blue{106} = 180$")
+assertSame("$3c^3-12c-8$")
